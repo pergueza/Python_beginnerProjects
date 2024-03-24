2f0bf67278add4d0fa44322f295f407bb93a3080
pergueza <d.pergueza@utp.edu.co>
2024-03-24 18:34:06 -0500

    Exceptions

--- Guess_Number/main.py
+++ Guess_Number/main.py
@@ -1,19 +1,60 @@
 import random
 
-maxNumber = int(input("Enter the maximum number that it can take"))
+
+def main():
+    print("Hello dear user, we going to play Guess the number.")
+    print("First, we need a integer bigger than 1, this number will be the",
+          "maximum number that it can take.")
+    showMenu()
+
+
+def showMenu():
+    maxNumber = 0
+    showOptions = True
+
+    while maxNumber <= 1:
+        try:
+            maxNumber = int(input("Enter a number: "))
+            if maxNumber <= 1:
+                print("Please enter a number bigger than 1.")
+        except ValueError:
+            print("Please enter a valid number.")
+
+    while (showOptions):
+        print("\nNow choose the option that you want.")
+        print("1. Guess the number of the system generated.")
+        print("2. System try to guess your number.")
+        print("3. Exit")
+        try:
+            option = int(input("Option: "))
+        except ValueError:
+            print("Please enter the number of the option.")
+
+        if option == 1:
+            guessUser(maxNumber)
+        elif option == 2:
+            guessSystem(maxNumber)
+        elif option == 3:
+            print("Goodbye!")
+            showOptions = False
+        else:
+            print("Invalid option. Please enter a number between 1 and 3")
 
 
 def guessUser(maxNumber):
     randomNumber = random.randint(1, maxNumber)
-    guess = 0
+    guess = int
 
     while guess != randomNumber:
-        guess = int(input(f"Guess a number between 1 and {maxNumber}: "))
+        try:
+            guess = int(input(f"Guess a number between 1 and {maxNumber}: "))
 
-        if guess < randomNumber:
-            print("Sorry, guess again. Too low.")
-        elif guess > randomNumber:
-            print("Sorry, guess again. Too high.")
+            if guess < randomNumber:
+                print("Sorry, guess again. Too low.")
+            elif guess > randomNumber:
+                print("Sorry, guess again. Too high.")
+        except ValueError:
+            print("Please enter a valid number.")
 
     print("Wow, congrats. You have guessed the number",
           "{} correctly".format(randomNumber))
@@ -23,4 +64,4 @@ def guessSystem(maxNumber):
     return 0
 
 
-guessUser(maxNumber)
+main()
